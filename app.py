38e1788925bc72367fade5eb8b2d1b201650e419
amailtoamal <amalchacko95@gmail.com>
2018-10-18 13:02:49 +0530

    654685

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 from flask import Flask, redirect, url_for, request, json
 from flask_pymongo import PyMongo
 from bson.json_util import dumps
+import jwt,time
 
 
-application = Flask(__name__)
 
-application.config["MONGO_URI"] = "mongodb://amal:amal1234@ds131743.mlab.com:31743/final_whistle"
-mongo = PyMongo(application)
+app = Flask(__name__)
+
+app.config["MONGO_URI"] = "mongodb://amal:amal1234@ds131743.mlab.com:31743/final_whistle"
+mongo = PyMongo(app)
 
 
-@application.route('/login', methods=['POST','GET'])
+@app.route('/login', methods=['POST', 'GET'])
 def login():
       x = mongo.db.users.find()
-      return dumps(x)
+      #return dumps(x)
+      encoded = jwt.encode({'some': 'payload', 'exp': int(time.time())}, 'secret', algorithm='HS256')
+      return encoded
 
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0', port=8080)
+    #app.run(host='0.0.0.0', port=8080)
+    app.run(host='127.0.0.1', port=5000, debug=True)

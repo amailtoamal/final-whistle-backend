df242d9fab55789e6193453d787a56cb83ac861c
amailtoamal <amalchacko95@gmail.com>
2018-10-18 15:27:59 +0530

    asdfsdfds

--- app.py
+++ app.py
@@ -3,20 +3,26 @@ from flask_pymongo import PyMongo
 from bson.json_util import dumps
 import jwt,time
 
-
-
 app = Flask(__name__)
 
 app.config["MONGO_URI"] = "mongodb://amal:amal1234@ds131743.mlab.com:31743/final_whistle"
 mongo = PyMongo(app)
 
+encoded = jwt.encode({'some': 'payload', 'exp': int(time.time())+10}, 'secret', algorithm='HS256')
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
       x = mongo.db.users.find()
       #return dumps(x)
-      encoded = jwt.encode({'some': 'payload', 'exp': int(time.time())}, 'secret', algorithm='HS256')
-      return encoded
+
+      try:
+            decoded=jwt.decode(encoded, 'secret', algorithms=['HS256'])
+      except jwt.ExpiredSignatureError:
+            return "Token expired"
+      except:
+            return "Token invalid"
+      # decoded = jwt.decode(encoded, 'secret', algorithm='HS256')
+      return str(decoded)
 
 
 if __name__ == "__main__":
